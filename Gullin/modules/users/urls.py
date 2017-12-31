2a6bfe23273cad69780b244b1bba1e369e64a24c
C0deZ Lee <c0dezlee@gmail.com>
2017-12-31 00:18:45 -0500

    Add token refresh

--- Gullin/modules/users/urls.py
+++ Gullin/modules/users/urls.py
@@ -14,8 +14,13 @@ user_signup = UserAuthViewSet.as_view({
 	'post': 'sign_up'
 })
 
+user_refresh = UserAuthViewSet.as_view({
+	'post': 'refresh'
+})
+
 urlpatterns = [
+	path('signup/', user_signup, name='user_signup'),
 	path('login/', user_login, name='user_login'),
+	path('refresh/', user_refresh, name='user_refresh'),
 	path('logout/', user_logout, name='user_logout'),
-	path('signup/', user_signup, name='user_signup')
 ]
