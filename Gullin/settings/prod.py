375f544e3aaf2ba13db924b851c6500c22a487bb
C0deZ Lee <c0dezlee@gmail.com>
2018-02-07 02:32:12 -0500

    Extend User expire time, delete message middleware

--- Gullin/settings/prod.py
+++ Gullin/settings/prod.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
 	'django.contrib.sessions',
-	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	# installed packages
 	'djcelery_email',
@@ -46,7 +45,6 @@ MIDDLEWARE = [
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
