d3bca783f79f655e50e75ac3a95fb57c3d16fe6e
C0deZ Lee <c0dezlee@gmail.com>
2018-03-28 18:13:19 -0400

    Remove Send_kyc_email

--- Gullin/modules/users/urls.py
+++ Gullin/modules/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import UserAuthViewSet, UserSignUpFollowUpViewSet, UserViewSet, send_kyc_email
+from .views import UserAuthViewSet, UserSignUpFollowUpViewSet, UserViewSet  # , send_kyc_email
 
 user_login = UserAuthViewSet.as_view({
 	'post' : 'log_in',
